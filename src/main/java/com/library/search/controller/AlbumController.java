8c4d87780f52b195d29f99bba4df0d18ffb8b4ac
Ierofantis <evil69669@gmail.com>
2020-06-22 12:53:39 +0300

    Add controllers with routes

--- src/main/java/com/library/search/controller/AlbumController.java
+++ src/main/java/com/library/search/controller/AlbumController.java
@@ -3,12 +3,24 @@ package com.library.search.controller;
 import com.library.search.service.AlbumService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping(value="/lib")
 public class AlbumController {
 
-    @Autowired
-    private AlbumService albumService;
+//    @Autowired
+//    private AlbumService albumService;
+
+    @PostMapping("/import")
+    public String response() {
+        System.out.println("import");
+        return "import";
+    }
+
+    @GetMapping("/view")
+    public String view(){
+        System.out.println("view");
+        return "view";
+    }
 }
